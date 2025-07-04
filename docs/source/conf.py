7b90d6cfb6ea2589cd160c34821a0075dd4e45ca
camaron <joel80858@gmail.com>
2025-07-04 13:19:26 -0600

    Styles

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
-
 project = 'Lumache'
 copyright = '2021, Graziella'
 author = 'Graziella'
@@ -10,7 +9,6 @@ release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -28,8 +26,6 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_static_path = ['_static']  # Habilita archivos estáticos (CSS)
+html_css_files = ['css/my-styles.css']  # CSS personalizado
